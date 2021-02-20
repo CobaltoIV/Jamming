79f7e8dff08ce0260f824cc40716fc5c7bf7118e
CobaltoIV <ricas1998@gmail.com>
2021-02-20 14:58:20 +0000

    First Commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Playlist } from "../Playlist/Playlist";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { SearchResults } from "../SearchResults/SearchResults";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <div className="App-playlist"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
