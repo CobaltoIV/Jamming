6fd19aa3ea90daa878e0725a9d98982b02f7910f
CobaltoIV <ricas1998@gmail.com>
2021-02-20 15:15:40 +0000

    Add Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,23 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Playlist } from "../Playlist/Playlist";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <div className="App-playlist"></div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <div className="App-playlist"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
