98d2dd8e9d1a741f6f74dae28d5ff87d78169c62
CobaltoIV <ricas1998@gmail.com>
2021-02-20 16:32:14 +0000

    Added removal and addition of tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Playlist } from "../Playlist/Playlist";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateResuls: [],
+      playlistName: "Chilling",
+      playlistTracks: [
+        {
+          name: "Young Jesus",
+          artist: "Portugal the Man",
+          album: "IDK",
+          id: 1,
+        },
+        {
+          name: "Kick it Still",
+          artist: "Portugal the Man",
+          album: "IDK",
+          id: 2,
+        },
+        {
+          name: "Black, Red, Blue",
+          artist: "Portugal the Man",
+          album: "Lols",
+          id: 3,
+        },
+        {
+          name: "Last",
+          artist: "Metallica",
+          album: "IDK",
+          id: 4,
+        },
+      ],
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlayListName = this.updatePlayListName.bind(this);
+  }
+
+  addTrack(track) {
+    const isInPlaylist = Boolean(
+      this.state.playlistTracks.find((t) => t.id === track.id)
+    );
+    if (isInPlaylist)
+      this.setState({ playlistTracks: this.state.playlistTracks.push(track) });
+  }
+
+  removeTrack(track) {
+    this.setState({
+      playlistTracks: this.state.playlistTracks.filter(
+        (t) => t.id !== track.id
+      ),
+    });
+  }
+
+  updatePlayListName(name) {
+    this.setState({ playlistName: name });
+  }
   render() {
     return (
       <div>
@@ -13,7 +68,19 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <div className="App-playlist"></div>
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults
+              searchResults={this.state.stateResuls}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlayListName}
+            />
+          </div>
         </div>
       </div>
     );
